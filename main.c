4f51cd39211c88e2acaa72e1d4b268ff129527ab
Arek001 <59519837+Arek001@users.noreply.github.com>
2021-10-31 20:24:32 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Arkadiusz Nowosiak\n");
+    printf("Arkadiusz\nNowosiak\n");
+    printf("Arkadiusz ");
+    printf("Nowosiak");
+    return 0;
+}
